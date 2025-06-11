f30be0e32410deb656ea820118f07c1860527465
YuriyPRO <pronagin2@gmail.com>
2025-06-11 11:15:52 +0300

    fix

--- src/components/OrderDetailsScreen.tsx
+++ src/components/OrderDetailsScreen.tsx
@@ -288,7 +288,7 @@ export default function OrderDetailsScreen({
         </View>
       )) || <Text style={styles.text}>Телефон не указан</Text>}
 
-      <Text style={styles.title}>Список товаров {qrCode}</Text>
+      <Text style={styles.title}>Список товаров</Text>
     </View>
   );
 
